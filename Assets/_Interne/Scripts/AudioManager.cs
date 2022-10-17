8a094945ce3138d42f5f445e5227d5eb99140e44
SuperMouton339 <carpentier339@gmail.com>
2022-10-17 16:44:06 -0400

    Ajout des GameObject pour l'audio manager avec son script et les variables + Ajout du TextMeshPro avec Fonctions Gagne et Perdre dans le GameManager

---
+++ Assets/_Interne/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //impact / explosion / musique / alarme / wipers / pew / ennemis / scienceFiction / moteur
+
+    [SerializeField] private AudioClip[] impacts;
+    [SerializeField] private AudioClip[] explosions;
+    [SerializeField] private AudioClip[] pews;
+    [SerializeField] private AudioClip[] ennemis;
+    [SerializeField] private AudioClip[] ambiants;
+
+
+    [SerializeField] private AudioClip musiqueEnJeu;
+    [SerializeField] private AudioClip alarme;
+    [SerializeField] private AudioClip essuiesGlace;
+    [SerializeField] private AudioClip moteurIdle;
+    [SerializeField] private AudioClip moteurPart;
+    [SerializeField] private AudioClip moteurMarche;
+
+    [SerializeField] public AudioSource audioSource;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Interne/Scripts/GameManager.cs
+++ Assets/_Interne/Scripts/GameManager.cs
@@ -1,32 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {   
-    private Vector3 target;
-    /*FIN */
-    
-
     //Reference
     
     [SerializeField] private Camera mainCamera;
 
     [SerializeField] private Transform canon;
 
-    // Update is called once per frame
+
+    [SerializeField] private float tempsExperienceMinute;
+
+
+    [SerializeField] private TextMeshProUGUI textMilieu;
     
+
+    [SerializeField] private int vies = 3;
+    // Update is called once per frame
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tempsExperienceMinute *= 60;
+        Invoke("Gagne", tempsExperienceMinute);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        VerifMort();
 
     }
 
@@ -36,8 +42,8 @@ public class GameManager : MonoBehaviour
         
     }
 
-    
-    void BougeCanonAvecAim()
+    //Fonction pour faire Bouger le canon selon le AIM
+    private void BougeCanonAvecAim()
     {
         Ray rayOrigin = Camera.main.ScreenPointToRay(GameObject.Find("Crosshair").transform.position);
         RaycastHit hitInfo;
@@ -59,5 +65,49 @@ public class GameManager : MonoBehaviour
                 canon.rotation = Quaternion.LookRotation(direction);
             }
         }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Fonctions pour Gagne ou perdre/Mourrir
+    private void VerifMort()
+    {
+        if (vies <= 0)
+        {
+            
+            textMilieu.text = "Vous êtes Mort!";
+            textMilieu.gameObject.SetActive(true);
+        }
+    }
+
+    private void Gagne()
+    {
+        textMilieu.text = "Vous avez Gagné!";
+        textMilieu.gameObject.SetActive(true);
     }
 }
