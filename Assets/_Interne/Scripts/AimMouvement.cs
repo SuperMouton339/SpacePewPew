dd4cee33c1111011995cef50b7cbb717a5fdce83
Alanis Bédard <alanisbedard@gmail.com>
2022-11-27 14:59:06 -0500

    Mise à jour après la Bêta
    Modifier la scène d'introduction pour enlever le canon.
    Modifier le script AimMouvement.

--- Assets/_Interne/Scripts/AimMouvement.cs
+++ Assets/_Interne/Scripts/AimMouvement.cs
@@ -11,28 +11,28 @@ public class AimMouvement : MonoBehaviour
     public string xAddress = "/teteHorizontal";
     public string yAddress = "/teteVertical";
     public string messageJoueur = "/joueurDetecte";
-    
+
     public OSCReceiver posReceiver;
     RectTransform UI_Element;
     private float x;
     private float y;
     private bool joueurDetecte;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-   
-        UI_Element = GameObject.Find("Crosshair").GetComponent<RectTransform>();
-        
-            posReceiver.Bind(xAddress, ReceiveMessageX);
-            posReceiver.Bind(yAddress, ReceiveMessageY);
-            posReceiver.Bind(messageJoueur, JoueurDetecte);
-        
-        
 
-        
+        UI_Element = GameObject.Find("Crosshair").GetComponent<RectTransform>();
+
+        posReceiver.Bind(xAddress, ReceiveMessageX);
+        posReceiver.Bind(yAddress, ReceiveMessageY);
+        //posReceiver.Bind(messageJoueur, JoueurDetecte);
+
+
+
+
         Cursor.visible = false;
     }
 
@@ -40,22 +40,21 @@ public class AimMouvement : MonoBehaviour
     void Update()
     {
 
-        if (joueurDetecte)
+
+        //Debug.Log("Je detecte le joueur");
+        UI_Element.anchoredPosition = new Vector3(x, y, 0f);
+
+        /*else
         {
-            //Debug.Log("Je detecte le joueur");
-            UI_Element.anchoredPosition = new Vector3(x, y, 0f);
-        }
-        else
-        {
-            //Debug.Log("Je ne detecte pas le joueur");
+            Debug.Log("Je ne detecte pas le joueur");
             transform.position = Input.mousePosition;
-        }
-        
+        }*/
+
     }
 
     void JoueurDetecte(OSCMessage message)
     {
-        if(message.Values[0].FloatValue > 0)
+        if (message.Values[0].FloatValue > 0)
         {
             joueurDetecte = true;
         }
@@ -63,20 +62,19 @@ public class AimMouvement : MonoBehaviour
         {
             joueurDetecte = false;
         }
-       
+
     }
 
     void ReceiveMessageX(OSCMessage message)
     {
         x = message.Values[0].FloatValue;
-        
+
     }
     void ReceiveMessageY(OSCMessage message)
     {
         y = message.Values[0].FloatValue;
-        
+
     }
 
 
 }
-
